4c87c75af04adc3c6f7b7962076d96f26e61f40d
Hamza Ed-doubi <hed-douibi@uoc.edu>
2019-12-18 17:41:03 +0100

    Parse input stream into JsonElement

--- runtime/src/main/java/com/xatkit/plugins/rest/platform/utils/Helpers.java
+++ runtime/src/main/java/com/xatkit/plugins/rest/platform/utils/Helpers.java
@@ -1,9 +1,15 @@
 package com.xatkit.plugins.rest.platform.utils;
 
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.Map;
 import java.util.stream.Collectors;
 
 import com.google.common.base.Splitter;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParseException;
+import com.google.gson.JsonParser;
+import fr.inria.atlanmod.commons.log.Log;
 
 public class Helpers {
 	
@@ -14,6 +20,17 @@ public class Helpers {
                 .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
     }
     
+    public static JsonElement parseJson(InputStream inputStream) {
+    	JsonElement jsonResponse  = null;
+    		try {
+    			jsonResponse = new JsonParser().parse(new InputStreamReader(inputStream));
+    		}
+    		catch (JsonParseException e) {
+    			Log.error(e, "Can't parse the response. See the attached exception");
+    		}
+    	
+		return jsonResponse;
+    }
    
 
 }
