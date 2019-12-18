d6863ededa49624a59696b985fdd3f3ce3f7b9de
Hamza Ed-doubi <hed-douibi@uoc.edu>
2019-12-18 17:43:31 +0100

    Implement PUT actions

---
+++ runtime/src/main/java/com/xatkit/plugins/rest/platform/action/PutJsonRequestWithFormData.java
@@ -0,0 +1,65 @@
+package com.xatkit.plugins.rest.platform.action;
+
+
+
+import static java.util.Objects.nonNull;
+
+import java.io.InputStream;
+import java.util.Map;
+
+import com.google.gson.JsonElement;
+import com.mashape.unirest.http.HttpResponse;
+import com.mashape.unirest.http.Unirest;
+import com.mashape.unirest.request.HttpRequest;
+import com.mashape.unirest.request.HttpRequestWithBody;
+import com.xatkit.core.session.XatkitSession;
+import com.xatkit.plugins.rest.platform.RestPlatform;
+import com.xatkit.plugins.rest.platform.utils.ApiResponse;
+import com.xatkit.plugins.rest.platform.utils.Helpers;
+
+import fr.inria.atlanmod.commons.log.Log;
+
+
+public class PutJsonRequestWithFormData extends RestRequest<JsonElement,JsonElement>  {
+
+   
+    public PutJsonRequestWithFormData(RestPlatform runtimePlatform, XatkitSession session, String restEndpoint, Map<String, Object> queryParams, Map<String, Object> pathParams, Map<String, String> headers, Map<String, Object> formParams ) {
+        super(runtimePlatform, session, MethodKind.PUT, restEndpoint, queryParams, pathParams,null, headers,formParams);
+      
+    }
+    
+    
+    
+    @Override
+    protected final ApiResponse<?> compute() throws Exception {
+    	HttpRequest request = Unirest.get(restEndpoint);
+    	if(nonNull(headers) && !headers.isEmpty())
+    			request.headers(headers);
+    	if(nonNull(queryParameters) && !queryParameters.isEmpty())
+    		request.queryString(queryParameters);
+    	if(nonNull(pathParameters) && !pathParameters.isEmpty())
+    		pathParameters.forEach((k,v)-> request.routeParam(k, v.toString()));
+    	if(nonNull(formParameters) && !formParameters.isEmpty())
+    		((HttpRequestWithBody)request).fields(formParameters);
+    	if(nonNull(requestBody))
+    		((HttpRequestWithBody)request).body(requestBody.toString());
+         
+    	Log.info("Sent PUT request on {0}", request.getUrl());
+    
+        HttpResponse<InputStream> response = request.asBinary();
+        return handleResponse(response.getHeaders(), response.getStatus(), response.getStatusText(), response.getBody(),Helpers::parseJson);
+    }
+
+
+
+	
+
+
+	
+
+
+
+
+    
+
+}
